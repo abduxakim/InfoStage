59b7c64f6e8af1e5378a6862f4c6ce14ab82ab27
Loki <mahmudovabduhakim4@gmail.com>
2025-10-07 18:19:32 +0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <>
+      <p className="bg-green-300">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
